706640d2c2a749f73224e2314be58599be390f00
Shukri Mohamed <Shukriiman83@gmail.com>
2025-07-05 15:10:26 +0800

    addedd Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,17 @@ import Classes from "./pages/Classes";
 import Messages from "./pages/Messages";
 import Profile from "./pages/Profile";
 import PageNotFound from "./pages/PageNotFound";
-import LoginPage from "./Features/Authentication/LoginPage";
-import SignUp from "./Features/Authentication/SignUp";
-
 import AuthPage from "./pages/AuthPage";
+import ProtectedRoute from "./Ui/ProtectedRoute";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<AppLayout />}>
+    <Route path="/auth" element={<AuthPage />} />
+        <Route path="/login" element={<Navigate to="/auth" replace />} />
+        <Route path="/signup" element={<Navigate to="/auth" replace />} />
+
+        <Route element={<ProtectedRoute><AppLayout /></ProtectedRoute>}>
           <Route index element={<Navigate replace to="dashboard" />} />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="students" element={<Students />} />
@@ -23,12 +25,9 @@ function App() {
           <Route path="messages" element={<Messages />} />
           <Route path="profile" element={<Profile />} />
           <Route path="auth" element={<AuthPage/>}/>
-
         </Route>
         <Route path="*" element={<PageNotFound />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUp />} />
-
+        
       </Routes>
     </BrowserRouter>
   );
