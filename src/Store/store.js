e81aeab3e6600215f86249bac66922854343c37a
shukri <shukriiman83@gmail.com>
2025-09-08 20:58:39 +0800

    addedd auth and student slice

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import studentReducer from "../Features/Students/studentSlice";
 import authReducer from '../Features/Authentication/AuthSlice.js'
-
+import classReducer from "../Features/Classess/classSlice";
 export const store = configureStore({
   reducer: {
     students: studentReducer,
-    auth: authReducer
+    auth: authReducer,
+    classes: classReducer,
   },
 });
 export default store;
