a91e59cdb513cbf8d1ad4bd8542e99c43df4d359
shukri <shukriiman83@gmail.com>
2025-09-08 20:54:03 +0800

    authenticationApi

--- src/API/AuthApi.js
+++ src/API/AuthApi.js
@@ -1,29 +1,46 @@
-//login function
-//signup function
 
-import { apiFetch } from "./client";
-
-
-export async function login(email, password) {
-  console.log("Logging in with:", { email, password });
-  return apiFetch("/api/auth/login", {
-    method: "POST",
-    body: JSON.stringify({ email, password }),
+import supabase from "./Supabase";
+export async function signup (name,email,password){
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        name,
+      }
+    }
   });
-}
-export async function signup(name, email, password) {
-  console.log("Signing up with:", { name, email, password });
-  return apiFetch("/api/auth/register", {
-    method: "POST",
-    body: JSON.stringify({ name, email, password }),
-  });
-}
 
-export async function getCurrentUser() {
-  return apiFetch("/api/auth/me");
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
 }
 
+export async function login(email,password){
+  const{data,error}=await supabase.auth.signInWithPassword({
+    email,
+    password
+  })
+  if(error){
+    throw new Error(error.message);
+  }
+  return data.user;
+}
 
+export async function getCurrentUser(){
+   const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+  const{data,error}=await supabase.auth.getUser();
+  console.log("current user", data?.user)
+  if(error){
+    throw new Error (error.message);
+  }
+  return data?.user
+}
 
-
-
+export async function logoutUser() {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
+}
